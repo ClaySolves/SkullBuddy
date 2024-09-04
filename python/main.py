213de4e86840161665fe512e29cabcd3957f7c19
clayScarCodes <scarboroclay@gmail.com>
2024-09-04 19:06:01 -0400

    file restrucutre

--- python/main.py
+++ python/main.py
@@ -1,28 +1,3 @@
-# TO DO:
-
-#the fix for item search not in right spot is to return more than 1 result first time,
-#search for string withing string, and if its YES, make plot map for dupes and refer to it
-
-
-
-# More bounds check for getting price. Need better algorithm than double search, get max, and list - undercut %
-# I should account for low listings as well (less than 3 for item check)
-
-# ideally the flow is->
-# base price with rareity
-# check all Attr 
-# if one attr +25% value or 100g, check best val with other attr selected
-# if condition happens again & >=epic, set aside for Manual Pricing 
-# else else get price and apply undercutPercent
-# dev CheckListings and < 5 throw value out
-
-# FIX item name detection, some itms not working
-# you could take screenshot to ONLY capture name for better reading
-
-# add statemachine that detects what part of game at, where need to go so you can start script whenever
-# statemachine accounts for what stash to sell from
-# statemachine accounts for how many selling slots left
-
 import math
 import sys
 import numbers
