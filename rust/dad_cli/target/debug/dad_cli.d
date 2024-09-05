138df2a9e35eaf250bc3dde914bd2129cdb0b441
clayScarCodes <scarboroclay@gmail.com>
2024-09-04 21:06:30 -0400

    files

--- rust/dad_cli/target/debug/dad_cli.d
+++
@@ -1,1 +0,0 @@
-C:\Main\darkAndDarker\autoTradingPost\rust\dad_cli\target\debug\dad_cli.exe: C:\Main\darkAndDarker\autoTradingPost\rust\dad_cli\src\main.rs
--- rust/dad_cli/target/debug/deps/dad_cli.d
+++
@@ -1,5 +0,0 @@
-C:\Main\darkAndDarker\autoTradingPost\rust\dad_cli\target\debug\deps\dad_cli.exe: src/main.rs
-
-C:\Main\darkAndDarker\autoTradingPost\rust\dad_cli\target\debug\deps\dad_cli.d: src/main.rs
-
-src/main.rs:
